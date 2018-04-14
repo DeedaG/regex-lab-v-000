dfe41b6ff7afde3118e2eb6d0c71ea6821ccc73c
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 22:52:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,10 +18,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    if text.match(/\b[A-Z]\w && \b[.?!.]\z/)
-    return true
-  else false
-  end
+  if text.match(/^[A-Z].*\.$/) == nil
+  return false
+else
+  return true
+end
 end
 
 def valid_phone_number?(phone)
