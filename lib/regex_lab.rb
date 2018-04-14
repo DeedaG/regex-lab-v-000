940224f1b220c58d87518ad405a6243e70c26f89
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 19:21:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,9 +5,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(/\A"un"\z"ing"/) == true
-  else false
-  end
+  text.scan(/\A"un"\z"ing"/)
+  
 end
 
 def words_five_letters_long(text)
