19c6487a65f6f12999bd25de31af2ce61c228a61
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 19:55:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z]/ && /\z.?/) == true
+  if text.scan(/^\w[A-Z]/ && /\z.?/) == true
   else false
   end
 end
