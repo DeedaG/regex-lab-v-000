c9a70d819ebb50740e59b87e1ac623f5a5d80bb4
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 21:51:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,7 +22,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   return false
 elsif text.scan(/(\b[A-Z]\w) || (\b[.?]\z)/)
   return false
-elsif text.scan(/(\b[A-Z]\w) && (\b[.?]\z)/)
+else
   return true  
   end
 end
