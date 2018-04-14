dde1c598ab8edab382b2f9b3f023b14ce02c442a
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 20:14:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,5 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d{10}+[()-]/)
+  if phone.scan(/\d{10} && [()-]/) || phone.scan(/\d{10}/) == true
+  else false
+  end
 end
