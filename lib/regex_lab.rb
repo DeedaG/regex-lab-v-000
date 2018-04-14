463f1091250bb248c905a1bcb9b12c4f748c61cd
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 19:50:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/\A[A-Z]/) && text.scan(/\z[.,!?:;]/) == true
+  if text.scan(/\A[A-Z]/) && text.scan(/\z[?]/) == true
   else false
   end
 end
