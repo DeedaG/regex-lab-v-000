3c7cbd652174aa0c0e8d0433c137d6f482d2383d
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 22:47:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    if text.match(/[A-Z]+ && \b[.?!.]\z/)
+    if text.match(/\b[A-Z]\w && \b[.?!.]\z/)
     return true
   else false
   end
