52d689b34151a9efd0366de99a579f16291ae8e2
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 19:19:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(\A"un")+(\z"ing") == true
+  if text.scan(\A"un"\z"ing") == true
   else false
 end
 
