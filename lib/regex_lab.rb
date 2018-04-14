f124049254ac2df9201be94e6c1b8dac5e7912a2
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-14 21:33:13 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/\b[A-Z]\w/) == nil
+  if text.scan(/\b[A-Z]\w/) &&
+     text.scan(/\z[.]/) == nil
   return false
 elsif text.scan(/\b[A-Z]\w/)
   return true
